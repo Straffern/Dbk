37347e9c1f663f92310e9612ea73ba808b0760e1
Alexander Flensborg <alex@flensborg.dev>
2025-03-31 21:53:54 +0200

    added http client

--- lib/dbk/http/client.ex
+++ lib/dbk/http/client.ex
@@ -1,4 +1,11 @@
 defmodule Dbk.Http.Client do
-  @callback request(method :: :get, url :: String.t(), headers :: list(), body :: String.t(), opts :: keyword()) ::
-    {:ok, %{status: integer(), data: String.t()}} | {:error, term()}
+  @callback request(
+              method :: :get | :post,
+              url :: String.t(),
+              headers :: list(),
+              body :: term(),
+              opts :: keyword()
+            ) ::
+              {:ok, %{status: integer(), body: term()}} | {:error, term()}
 end
+
--- lib/dbk/http/finch_client.ex
+++ lib/dbk/http/finch_client.ex
@@ -1,13 +1,42 @@
 defmodule Dbk.Http.FinchClient do
   @behaviour Dbk.Http.Client
 
+  require Logger
+
   def request(:get, url, headers, _body, _opts) do
     case Finch.build(:get, url, headers) |> Finch.request(Dbk.Finch) do
       {:ok, %Finch.Response{status: status, body: body}} ->
-        {:ok, %{status: status, data: body}}
-      
+        {:ok, %{status: status, body: decode_body(body)}}
+
       {:error, reason} ->
         {:error, reason}
     end
   end
+
+  def request(:post, url, headers, body, _opts) do
+    headers = [{"content-type", "application/json"} | headers]
+    encoded_body = Jason.encode!(body)
+
+    Logger.debug("Making POST request to #{url} with body: #{inspect(body)}")
+
+    case Finch.build(:post, url, headers, encoded_body) |> Finch.request(Dbk.Finch) do
+      {:ok, %Finch.Response{status: status, body: body}} ->
+        {:ok, %{status: status, body: decode_body(body)}}
+
+      {:error, reason} ->
+        Logger.error("Request failed: #{inspect(reason)}")
+        {:error, reason}
+    end
+  end
+
+  defp decode_body(""), do: nil
+
+  defp decode_body(body) when is_binary(body) do
+    Jason.decode!(body)
+  rescue
+    e ->
+      Logger.warning("Failed to decode JSON response: #{inspect(e)}")
+      body
+  end
 end
+
