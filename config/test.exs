2be3ef2acd67595b4899ee1a89944ecd0ba66245
Alexander Flensborg <alex@flensborg.dev>
2025-04-07 13:04:08 +0200

    disabled ash async for test

--- config/test.exs
+++ config/test.exs
@@ -5,6 +5,8 @@ config :dbk, Dbk.Repo,
   database: Path.expand("../dbk_test.db", Path.dirname(__ENV__.file)),
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :ash, :disable_async?, true
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :dbk, DbkWeb.Endpoint,
