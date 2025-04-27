6ff23e67f00b8dd595c702ac98aed2bc447eaa62
Alexander Flensborg <alex@flensborg.dev>
2025-04-27 13:47:17 +0200

    added municipal dimension

--- priv/repo/data_migrations/20250426221052_add_municipalities.exs
+++ priv/repo/data_migrations/20250426221052_add_municipalities.exs
@@ -1,4 +1,39 @@
 defmodule Numeri.Repo.Migrations.AddMunicipalities do
   use Ecto.Migration
+  alias Numeri.Repo
 
+  @json_path Path.join(__DIR__, "/20250426221052_add_municipalities/kommuner.json")
+
+  def change do
+    # Create dimension for municipalities
+    {_count, [ dimension ]} = 
+      Repo.insert_all("dimensions",
+        [ %{
+          name: "municipality",
+          description: "Danish municipalities"
+        } ], returning: [:id]
+      )
+
+    # Load and parse JSON data
+    json_data = 
+      @json_path
+      |> File.read!()
+      |> JSON.decode!()
+
+    # Insert each municipality as a value
+    rows =
+      Enum.map(json_data, fn municipality ->
+        %{
+          value: JSON.encode!(%{type: :string, value: municipality["navn"]}),
+          metadata: JSON.encode!(%{
+            "kode" => municipality["kode"],
+            "region" => municipality["region"]["navn"],
+            "bbox" => municipality["bbox"],
+            "visueltcenter" => municipality["visueltcenter"]
+          }),
+          dimension_id: dimension.id
+        }
+      end)
+    Repo.insert_all("values", rows)
+  end
 end
